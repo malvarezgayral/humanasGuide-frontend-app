f8e9d6dcf406410f44f742a9811033428f4108fb
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-09-16 06:33:08 -0300

    native selects for major and subjects in progress

--- src/components/MajorSelection.tsx
+++ src/components/MajorSelection.tsx
@@ -1,6 +1,28 @@
+import { InputLabel, NativeSelect } from "@mui/material";
+
 function MajorSelection() {
+    const majors = [
+        { value: 'historia', label: 'Historia' },
+        { value: 'geografia', label: 'Geografía' },
+        { value: 'rel_int', label: 'Relaciones internacionales' }
+    ];
+
     return <div>
-        here
+        {/* <h1 className="text-black">Selecciona tu carrera</h1> */}
+        <InputLabel htmlFor="pick-major" id="pickMajor-label">Selecciona tu carrera</InputLabel>
+        <NativeSelect
+            placeholder="Selecciona tu carrera"
+            inputProps={{
+                name: 'Selecciona tu carrera',
+                id: 'pick-major',
+            }}
+        >
+            {majors.map((major) => (
+                <option key={major.value} value={major.value}>
+                    {major.label}
+                </option>
+            ))}
+        </NativeSelect>
     </div>
 
 }
--- src/components/SubjectSelection.tsx
+++ src/components/SubjectSelection.tsx
@@ -1,6 +1,28 @@
+import { InputLabel, NativeSelect } from "@mui/material";
+
 function SubjectSelection() {
+    const subjects = [
+        { value: 'historia_general_social', label: 'Historia social general' },
+        { value: 'introd_economia', label: 'Introducción a la economía' },
+        { value: 'introd_sociologia', label: 'Introducción a la sociología' }
+    ];
+
     return <div>
-        oh here
+        {/* <h1 className="text-black">Selecciona la catedra</h1> */}
+        <InputLabel htmlFor="pick-Subject" id="pickSubject-label">Selecciona la cátedra</InputLabel>
+        <NativeSelect
+            placeholder="Selecciona la catedra"
+            inputProps={{
+                name: 'Selecciona la catedra',
+                id: 'pick-Subject',
+            }}
+        >
+            {subjects.map((subject) => (
+                <option key={subject.value} value={subject.value}>
+                    {subject.label}
+                </option>
+            ))}
+        </NativeSelect>
     </div>
 
 }
