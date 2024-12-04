10068fc43a792bd6c17f3f817e5b3a1023f6fb00
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-12-04 04:38:28 -0300

    instalacion de axios e integracion con el servidor para fetch de majors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,11 +5,28 @@ import CustomButton from "./CustomButton";
 import CustomSelection from "./CustomSelection";
 import { majors } from "@/constants/mockedData";
 import { getSubjects } from "@/services/api/getSubjects";
+import { fetchMajors } from "@/services/api/getMajors";
 
 function HomeComp() {
     const [pickedMajor, setPickedMajor] = useState('');
     const [subjects, setSubjects] = useState([])
     const [pickedSubject, setPickedSubject] = useState('');
+    const [fetchedMajors, setFetchedMajors] = useState(null);
+
+    console.log(fetchedMajors);
+
+    useEffect(() => {
+        const fetchingMajors = async () => {
+            try {
+                const data = await fetchMajors(); // manejamos la promesa con async await en este caso
+                setFetchedMajors(data); // updateamos el estado local del componente una vez que la respuesta esperada llegó
+            } catch (error) {
+                console.error("Error fetching majors:", error);
+            }
+        };
+    
+        fetchingMajors();
+    }, [])
 
     useEffect(() => {
         const subjectsTemp = getSubjects(pickedMajor);
@@ -30,15 +47,19 @@ function HomeComp() {
 
     return <div className="h-full w-full bg-primaryWhite py-8">
         <div className="w-full flex flex-col items-center">
-            <CustomSelection pickedValue={pickedMajor} setPickedValue={handleChangeMajor} title={'carrera'} iterableOptions={majors} />
-            {pickedMajor !== '' && subjects && subjects.length > 0 && <CustomSelection pickedValue={pickedSubject} setPickedValue={handleChangeSubject} title={'cátedra'} iterableOptions={subjects} />}
+            <CustomSelection pickedValue={pickedMajor} 
+            setPickedValue={handleChangeMajor} 
+            title={'carrera'} 
+            iterableOptions={majors} />
+            {pickedMajor !== '' && subjects && subjects.length > 0 && <CustomSelection pickedValue={pickedSubject} 
+            setPickedValue={handleChangeSubject} 
+            title={'cátedra'} 
+            iterableOptions={subjects} />}
         </div>
-
         <div className="flex flex-row justify-around py-5">
             <CustomButton title='Buscar aportes' />
             <CustomButton title='Subir aporte' />
         </div>
-
     </div>
 }
 
